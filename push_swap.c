ad0ad09e6cee1c9811cbc9e0529cb93590a558d4
jack-pepper <70756191+jack-pepper@users.noreply.github.com>
2024-12-13 17:46:24 +0100

    create struct s_stack

--- push_swap.c
+++ push_swap.c
@@ -8,6 +8,13 @@
 #include <stdio.h> // DEBUG!!!
 #include <unistd.h>
 
+typedef struct s_stack
+{
+	size_t	len;
+	int                    *content;
+        size_t	nb_elem;
+}                               t_stack;
+
 int	parse_args(int argc, char **argv);
 int	*store_args(int argc, char **argv);
 int	args_are_all_int(int argc, char **argv);
@@ -33,14 +40,28 @@ int	args_has_no_duplicate(int argc, char **argv);
  *
  */
 
+
+void    sa(t_stack stack_a)
+{
+	size_t	nb;
+	int	*stk_a;
+	
+	nb = stack_a.nb_elem;
+	stk_a = stack_a.content;
+        if (nb < 2)
+                return ;
+        stk_a[nb - 1] = stk_a[nb - 1] ^ stk_a[nb - 2];
+        stk_a[nb - 2] = stk_a[nb - 1] ^ stk_a[nb - 2];
+        stk_a[nb - 1] = stk_a[nb - 1] ^ stk_a[nb - 2];
+        ft_printf("stack_a[nb - 1] = %d - stack_a[nb - 2] = %d\n", stk_a[nb - 1], stk_a[nb - 2]);
+}
+
 int	main(int argc, char **argv)
 {
 	int	res;
-	int	*stack_a;
-	int	*stack_b;
-	size_t	stack_len;
+	t_stack	stack_a;
+	t_stack	stack_b;
 
-	stack_a = NULL;
 	if (argc < 3)
 	{
 		return (1);
@@ -54,25 +75,28 @@ int	main(int argc, char **argv)
 			return (1);
 		}
 		write(1, "Input is OK!\n", 14);
-		stack_a = store_args(argc, argv);
-		if (!stack_a)
+		stack_a.content = store_args(argc, argv);
+		if (!stack_a.content)
 		{
 			ft_printf("stack_a not mallocated: terminating...\n");
 			return (1);
 		}
-		stack_len = argc - 1;
-		ft_printf("Stack size: %d\n", stack_len);
-		stack_b = (int *)ft_calloc(stack_len, sizeof(int));
-		if (!stack_b)
+		stack_a.len = argc - 1;
+		ft_printf("Stack size: %d\n", stack_a.len);
+		stack_a.nb_elem = stack_a.len;
+		stack_b.content = (int *)ft_calloc(stack_a.len, sizeof(int));
+		if (!stack_b.content)
 		{
 			ft_printf("stack_b not mallocated: terminating...\n");
 			return (1);
 		}
-		stack_b[3] = 42;
-		ft_printf("Test: stack_b[0] {%d} stack_b[3] {%d}\n", stack_b[0], stack_b[3]);
+		stack_b.len = stack_a.len;
+		stack_b.nb_elem = 0;
+		sa(stack_a);
+		ft_printf("Test: stack_a[last] {%d} stack_a[antelast] {%d}\n", stack_a.content[stack_a.nb_elem - 1], stack_a.content[stack_a.nb_elem - 2]);
 	}
-	free(stack_a);
-	free(stack_b);
+	free(stack_a.content);
+	free(stack_b.content);
 	return (0);
 }
 
