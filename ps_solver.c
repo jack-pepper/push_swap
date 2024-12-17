15dc3e4372efe02322e6cdb75d787937616b44ea
jack-pepper <70756191+jack-pepper@users.noreply.github.com>
2024-12-17 23:06:37 +0100

    init small stacks handling

--- ps_solver.c
+++ ps_solver.c
@@ -6,16 +6,27 @@
 /*   By: mmalie <mmalie@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 17:48:27 by mmalie            #+#    #+#             */
-/*   Updated: 2024/12/17 21:20:36 by mmalie           ###   ########.fr       */
+/*   Updated: 2024/12/17 23:02:14 by mmalie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	is_ordered(t_stack *stack);
+void	handle_small_stacks(t_list *cmd_list, t_stack *stack_a);
+
 t_list	*ps_solver(t_list *cmd_list, t_stack *stack_a, t_stack *stack_b)
 {
 	t_list	*head;
 	t_list	*new_cmd;
+	
+	if (is_ordered(stack_a) == 0)
+		return (cmd_list);
+	if (stack_a->len <= 3)
+	{
+		handle_small_stacks(cmd_list, stack_a);
+		return (cmd_list);
+	}	
 
 	new_cmd = ft_lstnew("XXX");
 	if (!new_cmd)
@@ -29,4 +40,41 @@ t_list	*ps_solver(t_list *cmd_list, t_stack *stack_a, t_stack *stack_b)
         }
 	ft_printf("stack_a.len: %d - stack_b.len: %d\n", stack_a->len, stack_b->len);
 	return (cmd_list);
+}
+
+int	is_ordered(t_stack *stack)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < (stack->len - 1))
+	{
+		if (stack->content[i] < stack->content[i + 1])
+		{
+			ft_printf("is NOT ordered!\n");
+			return (1);
+		}
+		i++;
+	}
+	ft_printf("is ordered!\n");
+	return (0);
+}
+
+void	handle_small_stacks(t_list *cmd_list, t_stack *stack_a)
+{
+	t_list	*new_cmd;
+
+	if (stack_a->len == 2) // If prev test passed, should not be ordered
+		new_cmd = ft_lstnew("sa");
+	else if (stack_a->len == 3)
+	{
+		if (stack_a->highest_pos == 0 && stack_a->lowest_pos == 1)
+			new_cmd = ft_lstnew("sa");
+		else if (stack_a->highest_pos == 1 && stack_a->lowest_pos == 0)
+			new_cmd = ft_lstnew("rra");
+	}
+	if (!new_cmd)
+		return ;
+	ft_lstadd_back(&cmd_list, new_cmd);
+		return ;
 }
--- push_swap.c
+++ push_swap.c
@@ -1,6 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmalie <mmalie@student.42nice.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/17 23:05:34 by mmalie            #+#    #+#             */
+/*   Updated: 2024/12/17 23:06:00 by mmalie           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-/* TO DO:
- * - 
+/* TO DO NEXT:
+ * - Define other cases for handling small stacks 
  * -
  * */
 
@@ -61,7 +72,7 @@ int	main(int argc, char **argv)
 		//	return (1);
 		//}
 		// TEST
-	/*
+	
 		int *stk_a = stack_a.content;
 		int *stk_b = stack_b.content;
 		ft_printf("Before operation: \n");
@@ -72,8 +83,8 @@ int	main(int argc, char **argv)
 		ft_printf("[a1]: %d    [b1]: %d   \n", stk_a[1], stk_b[1]);
 		ft_printf("[a0]: %d    [b0]: %d   \n", stk_a[0], stk_b[0]);
 
-		ft_printf("push b! ");
-		pb(&stack_b, &stack_a);
+		ft_printf("rra! ");
+		rra(stack_a);
 
 		ft_printf("nb_elem a: %d - nb_elem b: %d\n ", stack_a.nb_elem, stack_b.nb_elem);	
 		ft_printf("[a4]: %d    [b4]: %d   \n", stk_a[4], stk_b[4]);
@@ -81,10 +92,10 @@ int	main(int argc, char **argv)
 		ft_printf("[a2]: %d    [b2]: %d   \n", stk_a[2], stk_b[2]);
 		ft_printf("[a1]: %d    [b1]: %d   \n", stk_a[1], stk_b[1]);
 		ft_printf("[a0]: %d    [b0]: %d   \n", stk_a[0], stk_b[0]);
-
+/*
 		ft_printf("push b! ");	
 		pb(&stack_b, &stack_a);
-
+		
 		ft_printf("nb_elem a: %d - nb_elem b: %d\n ", stack_a.nb_elem, stack_b.nb_elem);
 		ft_printf("[a4]: %d    [b4]: %d   \n", stk_a[4], stk_b[4]);
 		ft_printf("[a3]: %d    [b3]: %d   \n", stk_a[3], stk_b[3]);
