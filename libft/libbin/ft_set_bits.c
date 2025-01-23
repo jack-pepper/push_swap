63381c34787fb39d4a6b9ae90523423bdd3c7704
jack-pepper <70756191+jack-pepper@users.noreply.github.com>
2025-01-23 21:06:39 +0100

    add libft

---
+++ libft/libbin/ft_set_bits.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_set_bits.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmalie <mmalie@student.42nice.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/09 08:43:01 by mmalie            #+#    #+#             */
+/*   Updated: 2024/12/10 09:07:06 by mmalie           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+/*
+ * Sets a subset of bits in a number (from start_bit to end_bit) 
+ * to a given value. // ONGOING!
+ */
+unsigned int	ft_set_bits(unsigned int n, int st, int en, unsigned int val)
+{
+}
---
+++ libft/libbin/main.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmalie <mmalie@student.42nice.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 09:07:27 by mmalie            #+#    #+#             */
+/*   Updated: 2024/12/10 14:24:57 by mmalie           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+/*
+int	main(int argc, char **argv)
+{
+	int	argv1_int;
+	unsigned int	argv2_uint;
+	int	new_bin;
+	char	bin_get[33];
+
+	new_bin = 0;
+	argv1_int = 0;
+	if (argc != 3)
+	{
+		ft_printf("Error: arguments invalid.");
+	}
+	else
+	{
+		argv1_int = ft_atoi(argv[1]);
+		argv2_uint = ft_atoi(argv[2]);
+		ft_printf("Before operation: %d - ", argv1_int);
+		ft_show_bin(argv1_int, 32);
+		ft_get_bin(argv1_int, bin_get);
+		ft_printf("\nGot bin: %s\n", bin_get);
+		//new_bin = ft_set_bit(argv1_int, argv2_uint);
+		new_bin = ft_clear_bit(argv1_int, argv2_uint);
+		//new_bin = ft_toggle_bit(argv1_int, argv2_uint);
+		//new_bin = ft_is_bit_set(argv1_int, argv2_uint);
+		//new_bin = ft_count_set_bits(argv1_int);
+		//new_bin = ft_count_clear_bits(argv1_int);
+		//new_bin = ft_first_set_bit_pos(argv1_int);
+		//new_bin = ft_extract_bits(argv1_int);
+		//new_bin = ft_count_clear_bits(argv1_int);
+		//new_bin = ft_count_clear_bits(argv1_int);	
+		ft_printf("\nAfter operation: %d - ", new_bin);
+		ft_show_bin(new_bin, 32);
+	}
+	return (0);
+}
+*/
